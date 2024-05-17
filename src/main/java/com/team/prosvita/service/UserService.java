da6a19ae69183b70504e7f1bc9c31c3bbb6ad0d2
k84711 <kateryna.batii@gmail.com>
2024-05-17 20:28:51 +0300

    OPP-11 Implemented UserRepository methods to verify account existence by email and password correctness, and added service layer logic to handle these checks with error handling.

---
+++ src/main/java/com/team/prosvita/service/UserService.java
@@ -0,0 +1,21 @@
+package com.team.prosvita.service;
+
+import com.team.prosvita.repository.IUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
